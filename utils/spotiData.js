cc1c8b4b5b4783a4992cad78ca965a62b432cb48
kimonaristotelisvogt <kvogt@g.harvard.edu>
2023-01-26 21:36:56 -0500

    genre inclusion

--- utils/spotiData.js
+++ utils/spotiData.js
@@ -2,8 +2,11 @@ import { spotifyApiGet } from '../apis/spotifyApi'
 
 export const getSpotiData = async () => {
   const data = await spotifyApiGet('me')
-  const data_3 = await spotifyApiGet('me/top/tracks?limit=1&time_range=short_term')
-
+  //const data_3 = await spotifyApiGet('me/top/tracks?limit=1&time_range=short_term')
+  const data_3 = await spotifyApiGet('me/top/artists?limit=1')
+  // console.log('Hello')
+  // console.log(data_3.items[0].genres[0])
+  // console.log('Hello') 
   return data_3
 }
 
@@ -13,6 +16,6 @@ export const spotiData2Labels = async (data_3) => {
   //   genre: data_3.items[0].genres[0]
   // }
   return {
-    genre: 'jazz'
+    genre: data_3.items[0].genres[0]
   }
 }
